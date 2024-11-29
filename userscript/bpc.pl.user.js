f30b64d980495ac9edb282b45ec050728d3f076b
刘登峰 <liudf@xx-motor.com>
2024-11-29 23:54:44 +0800

    feat: update 20241129

---
+++ userscript/bpc.pl.user.js
@@ -0,0 +1,496 @@
+// ==UserScript==
+// @name            Bypass Paywalls Clean - pl
+// @version         3.9.2.0
+// @description     Bypass Paywalls of news sites
+// @author          magnolia1234
+// @downloadURL     https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=userscript/bpc.pl.user.js
+// @updateURL       https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=userscript/bpc.pl.user.js
+// @homepageURL     https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters
+// @supportURL      https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters
+// @license         MIT; https://gitflic.ru/project/magnolia1234/bypass-paywalls-clean-filters/blob/raw?file=LICENSE
+// @match           *://*.auto-swiat.pl/*
+// @match           *://*.businessinsider.com.pl/*
+// @match           *://*.forbes.pl/*
+// @match           *://*.komputerswiat.pl/*
+// @match           *://*.magazyn-kuchnia.pl/*
+// @match           *://*.newsweek.pl/*
+// @match           *://*.onet.pl/*
+// @match           *://*.parkiet.com/*
+// @match           *://*.pb.pl/*
+// @match           *://*.rp.pl/*
+// @match           *://*.wyborcza.biz/*
+// @match           *://*.wyborcza.pl/*
+// @match           *://*.wysokieobcasy.pl/*
+// @connect         archive.fo
+// @connect         archive.is
+// @connect         archive.li
+// @connect         archive.md
+// @connect         archive.ph
+// @connect         archive.vn
+// @grant           GM.xmlHttpRequest
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+window.setTimeout(function () {
+
+var domain;
+var mobile = window.navigator.userAgent.toLowerCase().includes('mobile');
+var csDoneOnce = true;
+var func_post;
+
+var overlay = document.querySelector('body.didomi-popup-open');
+if (overlay)
+  overlay.classList.remove('didomi-popup-open');
+var ads = 'div.OUTBRAIN, div[id^="taboola-"], div.ad-container, div[class*="-ad-container"], div[class*="_ad-container"], div.arc_ad, div[id^="poool-"]';
+hideDOMStyle(ads, 10);
+
+var pl_ringier_domains = ['auto-swiat.pl', 'businessinsider.com.pl', 'forbes.pl', 'komputerswiat.pl', 'newsweek.pl', 'onet.pl'];
+
+if (matchDomain('pb.pl')) {
+  let paywall = document.querySelector('div.paywall');
+  if (paywall) {
+    paywall.classList.remove('paywall');
+    let article_hidden = paywall.querySelector('section.o-article-content');
+    if (article_hidden)
+      article_hidden.removeAttribute('class');
+    let loader = document.querySelector('div.o-piano-template-loader-box');
+    removeDOMElement(loader);
+  }
+}
+
+else if (matchDomain(pl_ringier_domains)) {
+  if (matchDomain('businessinsider.com.pl')) {
+    let paywall = document.querySelector('div#content-premium-offer');
+    removeDOMElement(paywall);
+  }
+  let premium = document.querySelector('div.contentPremium[style]');
+  if (premium) {
+    premium.removeAttribute('class');
+    premium.removeAttribute('style');
+  }
+  let ads = 'div.adPlaceholder , div[class^="Ad"][class*="Placeholder_"], div[data-placeholder-caption], div[data-run-module$=".floatingAd"], aside[data-ad-container], aside.adsContainer, [class^="pwAds"], .hide-for-paying, div.onet-ad, div.bottomBar, ad-default';
+  hideDOMStyle(ads);
+}
+
+else if (matchDomain(['rp.pl', 'parkiet.com'])) {
+  let paywall = document.querySelector('div.paywallComp');
+  if (paywall) {
+    removeDOMElement(paywall);
+    let article = document.querySelector('div.article--content');
+    if (article) {
+      let url = window.location.href;
+      article.firstChild.before(googleSearchToolLink(url));
+    }
+  }
+}
+
+else if (matchDomain(['wyborcza.biz', 'wyborcza.pl', 'wysokieobcasy.pl', 'magazyn-kuchnia.pl'])) {
+  let url = window.location.href;
+  func_post = function () {
+    let empty_spans = document.querySelectorAll('figure > a > span:empty');
+    removeDOMElement(...empty_spans);
+  }
+  if (matchDomain(['wyborcza.biz', 'wyborcza.pl']))
+    getArchive(url, 'div.article--content-fadeout', {rm_attrib: 'class'}, 'div.container[class*="pt"]', '', 'div.body > div:not([style*="background-color:"]):not([old-position])');
+  else
+    getArchive(url, 'section.fade-out-article', {rm_attrib: 'class'}, 'article');
+  let ads = 'div[id^="adUnit"], div[id^="ads-"]';
+  hideDOMStyle(ads);
+}
+
+}, 1000);
+
+// General Functions
+
+function matchDomain(domains, hostname) {
+  var matched_domain = false;
+  if (!hostname)
+    hostname = window.location.hostname;
+  if (typeof domains === 'string')
+    domains = [domains];
+  domains.some(domain => (hostname === domain || hostname.endsWith('.' + domain)) && (matched_domain = domain));
+  return matched_domain;
+}
+
+function urlHost(url) {
+  if (/^http/.test(url)) {
+    try {
+      return new URL(url).hostname;
+    } catch (e) {
+      console.log(`url not valid: ${url} error: ${e}`);
+    }
+  }
+  return url;
+}
+
+function matchUrlDomain(domains, url) {
+  return matchDomain(domains, urlHost(url));
+}
+
+function setCookie(name, value, domain, path, days, localstorage_hold = false) {
+  var max_age = days * 24 * 60 * 60;
+  document.cookie = name + "=" + (value || "") + "; domain=" + domain + "; path=" + path + "; max-age=" + max_age;
+  if (!localstorage_hold)
+    window.localStorage.clear();
+}
+
+function removeDOMElement(...elements) {
+  for (let element of elements) {
+    if (element)
+      element.remove();
+  }
+}
+
+function hideDOMElement(...elements) {
+  for (let element of elements) {
+    if (element)
+      element.style = 'display:none !important;';
+  }
+}
+
+function hideDOMStyle(selector, id = 1) {
+  let style = document.querySelector('head > style#ext'+ id);
+  if (!style && document.head) {
+    let sheet = document.createElement('style');
+    sheet.id = 'ext' + id;
+    sheet.innerText = selector + ' {display: none !important;}';
+    document.head.appendChild(sheet);
+  }
+}
+
+function clearPaywall(paywall, paywall_action) {
+  if (paywall) {
+    if (!paywall_action)
+      removeDOMElement(...paywall);
+    else {
+      for (let elem of paywall) {
+        if (paywall_action.rm_class)
+          elem.classList.remove(paywall_action.rm_class);
+        else if (paywall_action.rm_attrib)
+          elem.removeAttribute(paywall_action.rm_attrib);
+      }
+    }
+  }
+}
+
+function waitDOMElement(selector, tagName = '', callback, multiple = false) {
+  new window.MutationObserver(function (mutations) {
+    for (let mutation of mutations) {
+      for (let node of mutation.addedNodes) {
+        if (!tagName || (node.tagName === tagName)) {
+          if (node.matches(selector)) {
+            callback(node);
+            if (!multiple)
+              this.disconnect();
+          }
+        }
+      }
+    }
+  }).observe(document, {
+    subtree: true,
+    childList: true
+  });
+}
+
+function waitDOMAttribute(selector, tagName = '', attributeName = '', callback, multiple = false) {
+  let targetNode = document.querySelector(selector);
+  if (!targetNode)
+    return;
+  new window.MutationObserver(function (mutations) {
+    for (let mutation of mutations) {
+      if (mutation.target.attributes[attributeName]) {
+        callback(mutation.target);
+        if (!multiple)
+          this.disconnect();
+      }
+    }
+  }).observe(targetNode, {
+    attributes: true,
+    attributeFilter: [attributeName]
+  });
+}
+
+function getSelectorLevel(selector) {
+  if (selector.replace(/,\s+/g, ',').match(/[>\s]+/))
+    selector = selector.replace(/,\s+/g, ',').split(',').map(x => x.match(/[>\s]+/) ? x + ', ' + x.split(/[>\s]+/).pop() : x).join(', ');
+  return selector;
+}
+
+function getArticleSrc(url, url_src, proxy, base64, selector, text_fail = '', selector_source = selector, selector_archive = selector) {
+  let url_fetch = url_src || url;
+  GM.xmlHttpRequest({
+    method: "GET",
+    url: url_fetch,
+    onload: function (response) {
+      let html = response.responseText;
+      if (proxy && base64) {
+        html = decode_utf8(atob(html));
+        selector_source = 'body';
+      }
+      let recursive;
+      if (url.startsWith('https://archive.')) {
+        if (url_fetch.includes('/https')) {
+          if (html.includes('<div class="TEXT-BLOCK"')) {
+            url_src = html.split('<div class="TEXT-BLOCK"')[1].split('</div>')[0].split('href="')[1].split('"')[0];
+            getArticleSrc(url, url_src, proxy, base64, selector, text_fail, selector_source, selector_archive);
+            recursive = true;
+          } else
+            html = '';
+        }
+      }
+      if (!recursive)
+        replaceDomElementExtSrc(url, url_src, html, proxy, base64, selector, text_fail, selector_source, selector_archive);
+    }
+  });
+}
+
+function replaceDomElementExt(url, proxy, base64, selector, text_fail = '', selector_source = selector, selector_archive = selector) {
+  let article = document.querySelector(selector);
+  if (!article)
+    return;
+  if (proxy) {
+    if (!text_fail) {
+      if (url.startsWith('https://archive.'))
+        text_fail = 'BPC > Try for full article text (no need to report issue for external site):\r\n';
+      else if (!matchUrlDomain(window.location.hostname, url))
+        text_fail = 'BPC > failed to load from external site:\r\n';
+    }
+    getArticleSrc(url, '', proxy, base64, selector, text_fail, selector_source, selector_archive);
+  } else {
+    let options = {};
+    fetch(url, options)
+    .then(response => {
+      let article = document.querySelector(selector);
+      if (response.ok) {
+        response.text().then(html => {
+          replaceDomElementExtSrc(url, '', html, false, base64, selector, text_fail, selector_source);
+        });
+      } else {
+        replaceTextFail(url, article, proxy, text_fail);
+      }
+    }).catch(function (err) {
+      false;
+    });
+  }
+}
+
+var selector_level = false;
+function replaceDomElementExtSrc(url, url_src, html, proxy, base64, selector, text_fail = '', selector_source = selector, selector_archive = selector) {
+  let article = document.querySelector(selector);
+  let article_link = document.querySelector(selector_archive);
+  let no_content_msg = '&nbsp;| no article content found! | :';
+  if (html) {
+    if (!proxy && base64) {
+      html = decode_utf8(atob(html));
+      selector_source = 'body';
+    }
+    let parser = new DOMParser();
+    window.setTimeout(function () {
+      if (url.startsWith('https://archive.') && url_src) {
+        let domain_archive = url.match(/^https:\/\/(archive\.\w{2})/)[1];
+        let pathname = new URL(url_src).pathname;
+        html = html.replace(new RegExp('https:\\/\\/' + domain_archive.replace('.', '\\.') + '\\/o\\/\\w+\\/', 'g'), '').replace(new RegExp("(src=\"|background-image:url\\(')" + pathname.replace('/', '\\/'), 'g'), "$1" + 'https://' + domain_archive + pathname);
+      }
+      let doc = parser.parseFromString(html, 'text/html');
+      if (selector_level)
+        selector_source = getSelectorLevel(selector_source);
+      let article_new = doc.querySelector(selector_source);
+      if (article_new) {
+        if (article && article.parentNode) {
+          if (url.startsWith('https://archive.')) {
+            let arch_dom = (selector_archive !== selector) ? (article_new.querySelector(selector_archive) || document.querySelector(selector_archive)) : article_new;
+            if (arch_dom) {
+              if (arch_dom.firstChild)
+                arch_dom = arch_dom.firstChild;
+              let arch_div = document.createElement('div');
+              arch_div.appendChild(archiveLink_renew(url_src));
+              arch_div.appendChild(archiveLink(window.location.href, 'BPC > Full article text fetched from (no need to report issue for external site):\r\n'));
+              arch_div.style = 'margin: 0px 0px 50px;';
+              arch_dom.before(arch_div);
+            }
+            let targets = article_new.querySelectorAll('a[target="_blank"][href^="' + window.location.origin + '"]');
+            for (let elem of targets)
+              elem.removeAttribute('target');
+            let invalid_links = article_new.querySelectorAll('link[rel*="preload"]:not([href])');
+            removeDOMElement(...invalid_links);
+          }
+          window.setTimeout(function () {
+            if (article.parentNode) {
+              article.parentNode.replaceChild(article_new, article);
+              if (func_post)
+                func_post();
+            }
+          }, 200);
+        }
+      } else
+        replaceTextFail(url, article_link, proxy, text_fail.replace(':', no_content_msg));
+    }, 200);
+  } else {
+    replaceTextFail(url, article_link, proxy, url_src ? text_fail.replace(':', no_content_msg) : text_fail);
+  }
+}
+
+function replaceTextFail(url, article, proxy, text_fail) {
+  if (text_fail && article) {
+    let text_fail_div = document.createElement('div');
+    text_fail_div.id = 'bpc_fail';
+    text_fail_div.setAttribute('style', 'margin: 0px 50px; font-weight: bold; color: red;');
+    text_fail_div.appendChild(document.createTextNode(text_fail));
+    if (proxy) {
+      if (url.startsWith('https://archive.')) {
+        text_fail_div = archiveLink(url.replace(/^https:\/\/archive\.\w{2}\//, ''), text_fail);
+      } else {
+        let a_link = document.createElement('a');
+        a_link.innerText = url;
+        a_link.href = url;
+        a_link.target = '_blank';
+        text_fail_div.appendChild(a_link);
+      }
+    }
+    if (article.firstChild)
+      article.firstChild.before(text_fail_div);
+    else
+      article.appendChild(text_fail_div);
+  }
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function archiveRandomDomain() {
+  let tld_array = ['fo', 'is', 'li', 'md', 'ph', 'vn'];
+  let tld = tld_array[randomInt(6)];
+  return 'archive.' + tld;
+}
+
+function getArchive(url, paywall_sel, paywall_action = '', selector, text_fail = '', selector_source = selector, selector_archive = selector) {
+  let url_archive = 'https://' + archiveRandomDomain() + '/' + url.split(/[#\?]/)[0];
+  let paywall = document.querySelectorAll(paywall_sel);
+  if (paywall.length) {
+    clearPaywall(paywall, paywall_action);
+    replaceDomElementExt(url_archive, true, false, selector, text_fail, selector_source, selector_archive);
+  }
+}
+
+function archiveLink(url, text_fail = 'BPC > Try for full article text (no need to report issue for external site):\r\n') {
+  return externalLink(['archive.today', archiveRandomDomain()], 'https://{domain}?run=1&url={url}', url, text_fail);
+}
+
+function archiveLink_renew(url, text_fail = 'BPC > Only use to renew if text is incomplete or updated:\r\n') {
+  return externalLink([new URL(url).hostname], '{url}/again?url=' + window.location.href, url, text_fail);
+}
+
+function googleSearchToolLink(url, text_fail = 'BPC > Try for full article text (test url & copy html (tab) code to [https://codebeautify.org/htmlviewer]):\r\n') {
+  return externalLink(['search.google.com'], 'https://search.google.com/test/rich-results?url={url}', encodeURIComponent(url), text_fail);
+}
+
+function externalLink(domains, ext_url_templ, url, text_fail = 'BPC > Full article text:\r\n') {
+  let text_fail_div = document.createElement('div');
+  text_fail_div.id = 'bpc_archive';
+  text_fail_div.setAttribute('style', 'margin: 20px; font-size: 20px; font-weight: bold; color: red;');
+  let parser = new DOMParser();
+  text_fail = text_fail.replace(/\[(?<url>[^\]]+)\]/g, function (match, url) {
+    return "<a href='" + url + "' target='_blank' style='color: red'>" + new URL(url).hostname + "</a>";
+  });
+  let doc = parser.parseFromString('<span>' + text_fail + '</span>', 'text/html');
+  let elem = doc.querySelector('span');
+  text_fail_div.appendChild(elem);
+  for (let domain of domains) {
+    let ext_url = ext_url_templ.replace('{domain}', domain).replace('{url}', url.split('?')[0]);
+    let a_link = document.createElement('a');
+    a_link.innerText = domain;
+    a_link.href = ext_url;
+    a_link.target = '_blank';
+    text_fail_div.appendChild(document.createTextNode(' | '));
+    text_fail_div.appendChild(a_link);
+  }
+  return text_fail_div;
+}
+
+function getArticleJsonScript() {
+  let scripts = document.querySelectorAll('script[type="application/ld+json"]');
+  let json_script;
+  for (let script of scripts) {
+    if (script.innerText.match(/"(articlebody|text)":/i)) {
+      json_script = script;
+      break;
+    }
+  }
+  return json_script;
+}
+
+function getNestedKeys(obj, key) {
+  if (key in obj)
+    return obj[key];
+  let keys = key.split('.');
+  let value = obj;
+  for (let i = 0; i < keys.length; i++) {
+    value = value[keys[i]];
+    if (value === undefined)
+      break;
+  }
+  return value;
+}
+
+function header_nofix(header, cond_sel = '', msg = 'BPC > no fix') {
+  if (header && typeof header === 'string')
+    header = document.querySelector(header);
+  if (header && !document.querySelector('div#bpc_nofix')) {
+    if (cond_sel) {
+      let elem = document.querySelectorAll(cond_sel);
+      if (elem.length)
+        removeDOMElement(...elem);
+      else
+        return false;
+    }
+    let nofix_div = document.createElement('div');
+    nofix_div.id = 'bpc_nofix';
+    nofix_div.style = 'margin: 20px; font-size: 20px; font-weight: bold; color: red;';
+    nofix_div.innerText = msg;
+    header.before(nofix_div);
+  }
+}
+
+function pageContains(selector, text) {
+  let elements = document.querySelectorAll(selector);
+  return Array.prototype.filter.call(elements, function (element) {
+    return RegExp(text).test(element.textContent);
+  });
+}
+
+function parseHtmlEntities(encodedString) {
+  let translate_re = /&(nbsp|amp|quot|lt|gt|deg|hellip|laquo|raquo|ldquo|rdquo|lsquo|rsquo|mdash);/g;
+  let translate = {"nbsp": " ", "amp": "&", "quot": "\"", "lt": "<", "gt": ">", "deg": "°", "hellip": "…",
+      "laquo": "«", "raquo": "»", "ldquo": "“", "rdquo": "”", "lsquo": "‘", "rsquo": "’", "mdash": "—"};
+  return encodedString.replace(translate_re, function (match, entity) {
+      return translate[entity];
+  }).replace(/&#(\d+);/gi, function (match, numStr) {
+      let num = parseInt(numStr, 10);
+      return String.fromCharCode(num);
+  });
+}
+
+function encode_utf8(str) {
+  return unescape(encodeURIComponent(str));
+}
+
+function decode_utf8(str) {
+  return decodeURIComponent(escape(str));
+}
+
+function ampToHtml() {
+  window.setTimeout(function () {
+    let canonical = document.querySelector('head > link[rel="canonical"][href]');
+    if (canonical)
+      window.location.href = canonical.href;
+  }, 1000);
+}
+
+function refreshCurrentTab() {
+  window.location.reload(true);
+}
+
+})();
